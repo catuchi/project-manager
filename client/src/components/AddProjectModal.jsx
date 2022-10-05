2ba5cbbc2bcc24705744ca9c017fd3cab55d4b46
Chibu <atuchibueze@gmail.com>
2022-10-05 19:58:58 -0400

    implement mutation to add project

--- client/src/components/AddProjectModal.jsx
+++ client/src/components/AddProjectModal.jsx
@@ -3,27 +3,27 @@ import { FaList } from "react-icons/fa";
 import { useMutation, useQuery } from "@apollo/client";
 import { GET_PROJECTS } from "../queries/projectQueries";
 import { GET_CLIENTS } from "../queries/clientQueries";
+import { ADD_PROJECT } from "../mutations/projectMutations";
 
 const AddProjectModal = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-  const [status, setStatus] = useState("new");
   const [clientId, setClientId] = useState("");
+  const [status, setStatus] = useState("new");
+
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: { name, description, clientId, status },
+    update(cache, { data: { addProject } }) {
+      const { projects } = cache.readQuery({ query: GET_PROJECTS });
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: [...projects, addProject] },
+      });
+    },
+  });
 
   // Get Clients for select
   const { loading, error, data } = useQuery(GET_CLIENTS);
-
-  // const [addClient] = useMutation(ADD_CLIENT, {
-  //   variables: { name, email, phone },
-  //   update(cache, { data: { addClient } }) {
-  //     const { clients } = cache.readQuery({ query: GET_CLIENTS });
-
-  //     cache.writeQuery({
-  //       query: GET_CLIENTS,
-  //       data: { clients: [...clients, addClient] },
-  //     });
-  //   },
-  // });
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -32,7 +32,7 @@ const AddProjectModal = () => {
       return alert("Please fill in all fields");
     }
 
-    // addClient(name, email, phone);
+    addProject(name, description, clientId, status);
 
     setName("");
     setDescription("");
